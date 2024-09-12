c78197e8c700dbec8a3a7adea5f8077b6db673ee
Si Nguyen <nguyenphusi13@gmail.com>
2024-09-12 17:37:58 +0700

    init

---
+++ app/components/Demo.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import {  useState } from 'react';
+import SlowList from './SlowList';
+
+export default function App() {
+  const [text, setText] = useState('');
+  return (
+    <>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <SlowList text={text} />
+    </>
+  );
+}
